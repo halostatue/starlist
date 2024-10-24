2a2936ce0ef38a85b4a587db6694e1291dd76d26
Austin Ziegler <austin@zieglers.ca>
2024-10-23 21:12:41 -0400

    chore: Add `remark-gfm` and `remark-github`

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,5 +1,7 @@
 import { remark } from 'remark'
 import toc from 'remark-toc'
+import gfm from 'remark-gfm'
+import github from 'remark-github'
 
 export const generate = async (doc: string): Promise<string> =>
-  String(await remark().use(toc).process(doc))
+  String(await remark().use(toc).use(gfm).use(github).process(doc))
